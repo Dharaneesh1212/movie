940a8ff934397e28f27be207e814f7e683d9b57d
dharaneesh1212 <dharun12122002@gmail.com>
2024-06-26 12:10:43 +0530

    mongoose

--- server/index.js
+++ server/index.js
@@ -2,17 +2,25 @@ import express from "express";
 import dotenv from "dotenv";
 import movieRoute from "./routes/movieRoute.js";
 import userRouter from "./routes/userRoute.js";
+import mongoose from "mongoose";
 
+// configs
 dotenv.config();
-
-const app = express();
-
 const PORT = process.env.PORT || 8000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
-
-app.listen(PORT, () => {
-  console.log(`server is running successfully http://localhost:${PORT}`);
-});
+const app = express();
 
 app.use("/api/movie", movieRoute);
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter);
+
+// connections
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running successfully http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error on connect to mongoDb", err);
+  });
