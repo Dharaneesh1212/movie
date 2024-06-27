bb88ff04c851a5318541228abf64247451c931e9
dharaneesh1212 <dharun12122002@gmail.com>
2024-06-27 20:21:40 +0530

    set

---
+++ server/controller/movieController.js
@@ -0,0 +1,4 @@
+import USER from "../models/userModel.js";
+
+// watched
+// wishlist
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const userRouter = express.Router();
 
+// signup
+// signin
+// forgot pswrd
+// reset pswrd
+
 userRouter.get("/auth", (req, res) => {
   res.send("Running authentication successfully");
   res.status(200);
