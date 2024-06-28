2c284566b97303e1db7e82b22837e92a7f2a2837
dharaneesh1212 <dharun12122002@gmail.com>
2024-06-28 12:09:06 +0530

    signin

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -25,6 +25,31 @@ userRouter.post("/signup", async (req, res) => {
 });
 
 // signin
+userRouter.post("signin", async (req, res)=>{
+  const {email, password} = req.body;
+  const user = await USER.findOne({email});
+  if(!user){
+    return res.json({message : "user does not registered"});
+  }
 
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if(!validPassword){
+    return res.json({message : "password does not match"});
+  }
+
+  const token = jwt.sign({ username: user.username }, process.env.KEY, {
+    expiresIn: "1h",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true, // Requires HTTPS
+    sameSite: "none",
+    maxAge: 360000, // Adjust based on your session timeout
+  });
+
+  return res.json({ status: true, message: "Login successful" });
+})
 
 export default userRouter;
